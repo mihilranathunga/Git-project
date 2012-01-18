c40ed7016c2c7f586e2ae20486b10b738ffbed6b
Mihil Ranathunga <mihilranathunga@gmail.com>
2012-01-18 22:17:54 +0530

    story 2: java file added

---
+++ gui.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Mihil Ranathunga
+ *
+ */
+public class gui {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is the first java git project");
+
+	}
+
+}
